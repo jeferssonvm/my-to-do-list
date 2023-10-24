5aadbe28fe71058786578bb8b486b46314b513b6
Jefersson Aldemar Velasquez Mendoza <jeferssonveme.97@gmail.com>
2023-10-24 06:02:19 -0500

    inicio del proyecto . creacion de la carpeta de react y eliminacion de archivos

---
+++ src/routers/MainRouter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {
+    BrowserRouter, Routes, Route, NavLink, Navigate,
+  } from 'react-router-dom';
+  
+export const MainRouter = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
